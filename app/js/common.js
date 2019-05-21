6610fc3cc3d418e0155237b415a7e22d5a486c27
sad <sadlykings@gmail.com>
2019-05-21 16:24:42 +0300

    ScrollBar

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,50 @@
 $(function() {
+    var sliderElem = document.getElementById('slider');
+    var thumbElem = sliderElem.children[0];
+    var scrolW = ($(window).width() - 249);
+    $(".items-container").scrollLeft(scrolW);
+    thumbElem.onmousedown = function(e) {
+        var thumbCoords = getCoords(thumbElem);
+        var shiftX = e.pageX - thumbCoords.left;
+        // shiftY здесь не нужен, слайдер двигается только по горизонтали
 
+        var sliderCoords = getCoords(sliderElem);
+
+        document.onmousemove = function(e) {
+            //  вычесть координату родителя, т.к. position: relative
+            var newLeft = e.pageX - shiftX - sliderCoords.left;
+
+            // курсор ушёл вне слайдера
+            if (newLeft < 0) {
+                newLeft = 0;
+            }
+            var rightEdge = sliderElem.offsetWidth - thumbElem.offsetWidth;
+            if (newLeft > rightEdge) {
+                newLeft = rightEdge;
+            }
+
+            thumbElem.style.left = newLeft + 'px';
+            $(".items-container").scrollLeft(newLeft);
+        };
+
+        document.onmouseup = function() {
+            document.onmousemove = document.onmouseup = null;
+        };
+
+        return false; // disable selection start (cursor change)
+    };
+
+    thumbElem.ondragstart = function() {
+        return false;
+    };
+
+    function getCoords(elem) { // кроме IE8-
+        var box = elem.getBoundingClientRect();
+
+        return {
+            top: box.top + pageYOffset,
+            left: box.left + pageXOffset
+        };
+
+    }
 });
